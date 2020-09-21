311ed9bab8f78b48c5ff80bab4ce3e3a0a10ff77
Bill Ma <mazengou@gmail.com>
2020-09-21 23:43:54 +0800

    finished basics & start the Bank class

---
+++ 01_C_sharp/01_03/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _03
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var account = new BankAccount("Bill", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        }
+    }
+}
---
+++ 01_C_sharp/01_03/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace _03
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal)
+        {
+        }
+    }
+}
